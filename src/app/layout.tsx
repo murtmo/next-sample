5007fa88abfb5894151ff1cdefd64d8649514be5
TamaoMiura <miuratamao.d@gmail.com>
2024-04-29 11:59:04 +0900

    set styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+
+// import "./globals.css";
+import '../styles/reset.css';
+import '../styles/main.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
